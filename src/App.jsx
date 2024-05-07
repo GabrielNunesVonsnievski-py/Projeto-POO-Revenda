3834e9b4fdf69733f2f557df0130ca6e57af9de4
Gabriel Nunes Vonsnievski <133696902+GabrielNunesVonsnievski-py@users.noreply.github.com>
2024-05-07 13:38:41 -0300

    primeiro commit front

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import axios from "axios";
+import "./App.css";
+
+
+function App() {
+  const [user, setUser] = useState({
+    nome: "",
+    email: "",
+    cargo_id: 0,
+  });
+  function inserirUsuario() {
+    axios.post("http:localhost:3000/usuarios", {
+      body: {
+        nome: user.nome,
+        email: user.email,
+        cargo_id: user.cargo_id,
+      },
+      header: {
+        "Content-Type": "Application/Json",
+      },
+    });
+  }
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <input
+          type="text"
+          id="nome"
+          value={user.nome}
+          placeholder="Nome"
+          onChange={(e) => setUser({ ...user, nome: e.target.value })}
+        />
+
+        <input
+          type="text"
+          id="email"
+          value={user.email}
+          placeholder="Email"
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+
+        <input
+          type="text"
+          id="cargo_id"
+          value={user.cargo_id}
+          placeholder="ID Cargo"
+          onChange={(e) =>
+            setUser({ ...user, cargo_id: parseInt(e.target.value) })
+          }
+        />
+        <button style={{backgroundColor: 'cyan'}} onClick={() => inserirUsuario()}>
+          Inserir Cliente
+        </button>
+      </div>
+
+    </>
+  )
+}
+
+export default App;
